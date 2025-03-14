91b8e0e715408cf521b0227309bb6c1a9f2a624d
jeong da-young <bja05008@gmail.com>
2025-03-14 12:40:32 +0900

    라이센스 에이전트 api

--- controller/license.go
+++ controller/license.go
@@ -32,3 +32,57 @@ func (c *Controller) License(ctx *gin.Context) {
 	}
 	ctx.IndentedJSON(http.StatusOK, license_data)
 }
+
+// IsLicenseExpired godoc
+//
+//	@Summary                IsLicenseExpired
+//	@Description    		라이센스 만료일을 조회합니다.
+//	@Tags                   License
+//	@Accept                 x-www-form-urlencoded
+//	@Produce                json
+//	@Success                200     {object}        LicenseList
+//	@Failure                400     {object}        httputil.HTTP400BadRequest
+//	@Failure                404     {object}        httputil.HTTP404NotFound
+//	@Failure                500     {object}        httputil.HTTP500InternalServerError
+//	@Router                 /api/v1/license/isLicenseExpired [get]
+func (c *Controller) IsLicenseExpired(ctx *gin.Context) {
+	ctx.Header("Access-Control-Allow-Origin", "*")
+
+	license_data, err := license.IsLicenseExpired("password", "salt")
+	if err != nil {
+		utils.FancyHandleError(err)
+		httputil.NewError(ctx, http.StatusInternalServerError, err)
+		return
+	}
+	ctx.IndentedJSON(http.StatusOK, license_data)
+}
+
+// ControlHostAgent godoc
+//
+//	     @Summary                ControlHostAgent
+//	     @Description    		Mold Agent를 제어합니다.
+//			@param			action	path	string	true	"Agent action(start, stop)"
+//	     @Tags                   License
+//	     @Accept                 x-www-form-urlencoded
+//	     @Produce                json
+//	     @Success                200     {object}        LicenseList
+//	     @Failure                400     {object}        httputil.HTTP400BadRequest
+//	     @Failure                404     {object}        httputil.HTTP404NotFound
+//	     @Failure                500     {object}        httputil.HTTP500InternalServerError
+//	     @Router                 /api/v1/license/controlHostAgent/{action} [get]
+func (c *Controller) ControlHostAgent(ctx *gin.Context) {
+	ctx.Header("Access-Control-Allow-Origin", "*")
+	action := ctx.Param("action")
+	if action == "start" {
+		license.ControlHostAgent(true) //agent 시작
+	} else {
+		license.ControlHostAgent(false) //agent 정지
+	}
+	// license_data, err := license.ControlHostAgent("false")
+	// if err != nil {
+	// 	utils.FancyHandleError(err)
+	// 	httputil.NewError(ctx, http.StatusInternalServerError, err)
+	// 	return
+	// }
+	ctx.IndentedJSON(http.StatusOK, "license_data")
+}
